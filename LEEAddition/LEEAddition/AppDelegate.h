b9f8db35867b07945ea5757ecab8c15908ce694c
李江波 <584556825@qq.com>
2017-06-17 13:19:19 +0800

    项目初始化

---
+++ LEEAddition/LEEAddition/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LEEAddition
+//
+//  Created by 李江波 on 2017/6/17.
+//  Copyright © 2017年 lijiangbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
